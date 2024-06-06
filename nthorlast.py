7f9b0d99faa225479e71e53e8f26ab7a2aedc5da
Ghazal Hafezi <ghazalhafezi@Ghazals-Laptop.local>
2024-06-06 14:40:33 +0330

    nth or last with tests

--- nthorlast.py
+++ nthorlast.py
@@ -11,6 +11,9 @@ from itertools import islice
 _marker= object()
 
 def nth_or_last(iterable,n , default=_marker):
-    return last(islice(iterable , n+1)   , default=default)      # aval ino minevisim   `return islice(iterable , n)`     in manteghe slice e ta shomarei k beehesh midi tiike mikone . vali inja function nth_or_last ()  mikhad tebghe index kar kone pas code ro intori mikonim. ` return islice(iterable , n+1) `      hala k slice vasamon listemono ta indexee morede nsazaremon tike karde, nobate ine k akhario return konim. k miaym az function `last` k ghblan khodemon hminja sakhtim estefade mikonim. aval importesh mikonim vaa badesh kole dastanaro midim bhsh
-
+     # aval ino minevisim   `return islice(iterable , n)`     in manteghe slice e ta shomarei k beehesh midi tiike mikone . vali inja function nth_or_last ()  mikhad tebghe index kar kone pas code ro intori mikonim. ` return islice(iterable , n+1) `      hala k slice vasamon listemono ta indexee morede nsazaremon tike karde, nobate ine k akhario return konim. k miaym az function `last` k ghblan khodemon hminja sakhtim estefade mikonim. aval importesh mikonim vaa badesh kole dastanaro midim bhsh
+    result = last(islice(iterable, n + 1), default=default)
+    if result is _marker:
+        raise ValueError("Empty iterable and no default value provided")
+    return result
 
--- test_northorlast.py
+++ test_northorlast.py
@@ -6,3 +6,17 @@ class NthOrLastTest(unittest.TestCase):
     def test_basic(self):
         self.assertEqual(nthorlast.nth_or_last(range(4) , 2) ,2)
         self.assertEqual(nthorlast.nth_or_last(range(2) , 2) ,1)
+        
+        
+    def test_default_value(self):
+        default=60
+        self.assertEqual(nthorlast.nth_or_last(range(2) , 1 , default), 1)
+        self.assertEqual(nthorlast.nth_or_last([] ,1 , default), default)
+        
+    def test_empty_iterable_no_default(self):
+        self.assertRaises(ValueError, lambda:nthorlast.nth_or_last([],0))    
+
+
+
+if __name__ == "__main__":
+    unittest.main()
